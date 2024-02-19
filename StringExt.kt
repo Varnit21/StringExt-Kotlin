f5d9f03ad9314a367e163e43a46b3780c7086673
Varnit 69 <79410063+Varnit21@users.noreply.github.com>
2024-02-19 15:36:28 +0530

    Updated StringExt.kt

--- StringExt.kt
+++ StringExt.kt
@@ -1,5 +1,7 @@
 // StringExt.kt
 
+import java.util.regex.Pattern
+
 fun String.reverse(): String {
     return this.reversed()
 }
@@ -19,4 +21,24 @@ fun String.capitalizeWords(): String {
 
 fun String.truncate(length: Int, suffix: String = "..."): String {
     return if (this.length > length) this.substring(0, length) + suffix else this
+}
+
+fun String.findAndReplace(find: String, replace: String): String {
+    return this.replace(find, replace)
+}
+
+fun String.toCamelCase(): String {
+    return this.split("\\s+").joinToString("") { it.capitalize() }
+}
+
+fun String.extractEmails(): List<String> {
+    val emailRegex = Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
+    val matcher = emailRegex.matcher(this)
+    val emails = mutableListOf<String>()
+
+    while (matcher.find()) {
+        emails.add(matcher.group())
+    }
+
+    return emails
 }
